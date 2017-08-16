9e81d47b590c865999cc632da981cdbf42f349f1
Thomas Siwczak <thomas.siwczak@gmail.com>
2017-08-16 14:37:59 +0200

    3.1.0 - add required fields

---
+++ templates/fieldTypes.js
@@ -0,0 +1,26 @@
+module.exports = {
+	rString: {
+		name: '{ type: String, required: true }'
+	},
+	rNumber: {
+		name: '{ type: Number, required: true }'
+	},
+	rDate: {
+		name: '{ type: Date, required: true }'
+	},
+	rBoolean: {
+		name: '{ type: Boolean, required: true }'
+	},
+	string: {
+		name: '{ type: String }'
+	},
+	number: {
+		name: '{ type: Number }'
+	},
+	date: {
+		name: '{ type: Date }'
+	},
+	boolean: {
+		name: '{ type: Boolean }'
+	},
+};
--- templates/model.js
+++ templates/model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var {schemaName} = new Schema({fields});
+var {schemaName} = new Schema({fields}, { timestamps: true });
 
 module.exports = mongoose.model('{modelName}', {schemaName});
